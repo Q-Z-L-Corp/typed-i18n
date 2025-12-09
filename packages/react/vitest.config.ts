6ed52551e434c6bbca85a1037594b258b2973433
Errong Leng <errong.leng@gmail.com>
2025-12-08 20:27:26 -0800

    configure vitest to run against current source code

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@qzlcorp/typed-i18n": path.resolve(__dirname, "../core/src/index.ts"),
+		},
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
--- packages/vue/vitest.config.ts
+++ packages/vue/vitest.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			"@qzlcorp/typed-i18n": path.resolve(__dirname, "../core/src/index.ts"),
+		},
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
